0526b3b505598a6d26b049ddb5b2831573475e1c
Parth Madhvani <95546695+ParthMadhvani2@users.noreply.github.com>
2023-03-21 23:17:56 +0530

    Add files via upload

---
+++ functions/func0.cpp
@@ -0,0 +1,24 @@
+// What is function :
+
+/*
+    --> syntax of function :
+    returntype functionname(parameter1, parameter2,....){
+        function body
+    }
+
+    Eg : 
+
+    function to add two numbers is :
+
+    //declaration of a function
+    int add(int num1, int num2){
+        int sum = num1 + num2;
+        return sum;
+    }
+    int main(){
+        int a = 2;
+        int b = 3;
+        cout << add(a,b) << endl; // calling of a function
+        return 0;
+    }
+*/
