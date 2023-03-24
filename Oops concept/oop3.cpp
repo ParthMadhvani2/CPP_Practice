8355a093af481f2b54f2deb4ea448310c984a15f
Parth Madhvani <95546695+ParthMadhvani2@users.noreply.github.com>
2023-03-24 15:51:00 +0530

    Add files via upload

---
+++ Oops concept/oop3.cpp
@@ -0,0 +1,25 @@
+// create a class of cars :
+
+#include<iostream>
+using namespace std;
+
+//create a class for cars
+class cars{
+    int year_of_manufacturing;
+    char model;
+    int top_speed;
+    char colour;
+    int engine_power_bhp;
+
+};
+
+int main(){
+    system("clear");
+
+    //objects
+    cars mercedes;
+
+    
+    cars bmw;
+    cars ferrari;
+}
