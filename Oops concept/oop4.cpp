bd5aaed61721fe4aeb2f193d4ad1953b514d6293
Parth Madhvani <95546695+ParthMadhvani2@users.noreply.github.com>
2023-03-24 15:51:14 +0530

    Add files via upload

---
+++ Oops concept/oop4.cpp
@@ -0,0 +1,28 @@
+//
+
+/*
+
+Data encapsulation is supported with “class”. The class consists of both data and functions. 
+The data in a class is called a member, while functions in the class are called methods. 
+
+Data Members:- 
+The variables which are declared in any class by using any fundamental data types (like int, char, float, etc.) 
+or derived data types (like class, structure, pointer, etc.) are known as Data Members.
+
+Methods:- 
+A method is the equivalent of a function in object-oriented programming that is used inside classes. 
+The methods are the actions that perform operations. 
+A method accepts parameters as arguments, manipulates these, and then produces an output when the method is called on an object. 
+
+Constructor:- 
+Constructors are special class functions that perform the initialization of every object. 
+In C++, the constructor is automatically called when an object is created. 
+It is a special method of the class because it does not have any return type. 
+It has the same name as the class itself.
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){}
