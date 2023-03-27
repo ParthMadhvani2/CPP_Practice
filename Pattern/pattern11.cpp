746c760fe30131f9dbeebaa884b6ce6db4be4566
Parth Madhvani <95546695+ParthMadhvani2@users.noreply.github.com>
2023-03-27 19:18:02 +0530

    Add files via upload

---
+++ Pattern/pattern11.cpp
@@ -0,0 +1,33 @@
+// Print star pattern
+
+/*
+    12345
+    1234
+    123
+    12
+    1
+    
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    system("clear");
+    int n;
+    cin >> n;
+    cout << endl;
+    int i=1;
+    
+    while(i<=n){
+        int j=1;
+        while(j<=n-i+1){
+            cout << j<< " ";
+            j++;
+
+        }
+        
+        cout << endl;
+        i++;
+    }
+}
